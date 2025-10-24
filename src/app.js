90a9d746326cec35f88180f7825b9f0ec934c0b1
codinaven <anshunit2023@gmail.com>
2025-10-24 22:59:13 +0530

    this is the first coommit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
-
+const { connectDB } = require("./config/database.js");
 const app = express();
+const { User } = require("./models/user.js");
 
-app.use("/test", (req, res) => {
-  res.send("hellow from the server");
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  //NOTE: Creating a new instance of the user model
+  const user = new User(req.body);
+  //NOTE: Saving the data in database
+  try {
+    await user.save();
+    res.send("user added successfully ");
+  } catch (err) {
+    res.status(400).send("Error saving the data " + err.message);
+  }
 });
 
-app.listen(3000, () => {
-  console.log("your sever is listening at port 3000");
-});
+connectDB()
+  .then(() => {
+    console.log("database connnection establised ");
+    app.listen(3000, () => {
+      console.log("your sever is listening at port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("databae cannot be connected", err.message);
+  });
